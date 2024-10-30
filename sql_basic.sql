bdaae07d291baa0e85f3d4b666a3787495a10480
HoriaAnton <horiaantonio@gmail.com>
2024-10-30 21:01:55 +0200

    update

--- sql_basic.sql
+++ sql_basic.sql
@@ -12,7 +12,7 @@ create table users(
     address VARCHAR(200)
 )
 
---cum sa inserezi in tab
+--cum sa inserezi in tabel
 INSERT INTO users (name, mail, phone, date_of_birth, address) VALUES
                                                                   ('John Doe', 'john.doe@example.com', 123456, '1980-05-15', '123 Main St, City, Country'),
                                                                   ('Jane Smith', 'jane.smith@example.com', 23456, '1990-06-20', '456 Elm St, Town, Country'),
@@ -49,7 +49,7 @@ SELECT * FROM users WHERE name != 'Maria';
 
 <, >, <=, >=, și <>
 
-Ex: SELECT * FROM users WHERE phone < 200000;  
+SELECT * FROM users WHERE phone < 200000;  
 
 
 --conditii multiple 
@@ -77,4 +77,40 @@ SELECT * FROM users WHERE name LIKE 'A_';
 --IS NULL si IS NOT NULL este utilizat pentru a verifica daca o coloana are sau nu valori lipsa
 SELECT * FROM orders WHERE phone IS NULL;
 SELECT * FROM orders WHERE phone IS NOT NULL;
+
+
+
+
+
+--MODIFICARE DATA CU SQL
+--inserare in tabel
+
+INSERT INTO users  VALUES (135,'Antonio','antonio@gmail.com',0761731,1990-03-15,'Bucuresti');
+
+
+--inserare intr un singur rand cu valori in orice ordine
+INSERT INTO users (name, mail, phone, date_of_birth, address) VALUES ('Lucasandru', 'Lucasandru@gmail.com', 0563454,1442-12-30,'Spania');
+
+INSERT INTO  books (title, author, isbn, year_realeased) VALUE  ('Luceafarul','Eminescu',942343243,1920-12-10);
+
+
+
+-- updatin la toate randurile dintr un tabel
+UPDATE users SET phone = 107229238;
+
+
+--update la mai multe coloane la toate randurile
+
+UPDATE users SET name = 'Anony', phone = 65463634;
+
+
+--update la o linie anume
+UPDATE  users SET name = 'Beatrice' WHERE name = 'Maria';
+
+
+--pentru a da delete la toate randurile dintr un tabel 
+DELETE FROM users;
+
+
+--pentru a sterege un anumit rand 
 
