1a6e5be65356b157e8085fc2a1bfa8443054d8a4
HoriaAnton <horiaantonio@gmail.com>
2024-11-20 17:07:15 +0200

    UPDATE

--- Querying Relationa/Querying Relational-exercitii.sql
+++ Querying Relationa/Querying Relational-exercitii.sql
@@ -1,28 +1,36 @@
---afisare nume studenti si numele cartiilor  asociate
-select student.first_name,book.book_name 
-from student JOIN  book on 
-student.id = book.student_id
-
-
---afisare nume studenti si nume carti dupa idul 1 si 3
-SELECT first_name, book_name FROM student
-
-JOIN book on student.id = book.student_id where student_id = 1 or student_id =2;
+--afissare asocierilor dintre studenti si cartile lor
+SELECT student.first_name, book.book_name     --ce vrei sa iti afiseze
+FROM student JOIN book                        --de unde sa isi ia datele
+ON student.id = book.student_id               --sa verifice asocierea de date
 
 
 
---afisare nume cursului si cine este inscris
+--afisare asocierii datelor din cele doua tabele si la final e si o conditie
+SELECT student.first_name, book.book_name FROM student
+JOIN book ON student.id = book.student_id WHERE student_id = 1 
+
+
+
+--afisare numele cursului si cine este inscris la el, asociere
 SELECT  course.name, student.first_name FROM enrolment
 
 JOIN student ON  student_id = student.id
 JOIN course ON course_id = course.id
 
 
---afisarea studentilor care au acelasi id la student_id de la book si a studentilor care nu au nici o carte
-SELECT first_name, book_name                          --ce coloane sa se selecteze
-FROM student  
-left JOIN book on                    
-student.id = book.student_id      
+--afisare numele userilor care au id ul asociat cu student_id din book iar apoi si cei care nu au
+SELECT first_name, book_name
+FROM student left JOIN book on
+    student.id = book.student_id;
 
 
+--afisarea cartilor care au student_idul asociat cu id ul userilor iar apoi si cei care nu au
+SELECT  book_name, first_name
+FROM student RIGHT JOIN book on
+    student.id = book.student_id;
 
+
+--returneaza toate valorile de la ambele tabele (cu duplicari)
+SELECT book_name FROM book
+UNION
+SELECT id FROM  student`1
